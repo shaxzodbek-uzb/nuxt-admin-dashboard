3a0c08ca5a40f0d5d84924415d650cfb7c62533e
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2021-10-28 15:31:15 +0500

    init

---
+++ components/ProfileDropdown.vue
@@ -0,0 +1,84 @@
+<template>
+  <!-- Profile dropdown -->
+  <div class="ml-3 relative">
+    <div>
+      <button
+        id="user-menu-button"
+        type="button"
+        class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+        aria-expanded="false"
+        aria-haspopup="true"
+        @click="showMenu"
+      >
+        <span class="sr-only">Open user menu</span>
+        <img
+          class="h-8 w-8 rounded-full"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt
+        />
+      </button>
+    </div>
+
+    <!--
+                Dropdown menu, show/hide based on menu state.
+
+                Entering: "transition ease-out duration-100"
+                  From: "transform opacity-0 scale-95"
+                  To: "transform opacity-100 scale-100"
+                Leaving: "transition ease-in duration-75"
+                  From: "transform opacity-100 scale-100"
+                  To: "transform opacity-0 scale-95"
+    -->
+    <div
+      v-show="isMenuShow"
+      class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+      role="menu"
+      aria-orientation="vertical"
+      aria-labelledby="user-menu-button"
+      tabindex="-1"
+    >
+      <!-- Active: "bg-gray-100", Not Active: "" -->
+      <a
+        id="user-menu-item-0"
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabindex="-1"
+      >Your Profile</a>
+
+      <a
+        id="user-menu-item-1"
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabindex="-1"
+      >Settings</a>
+
+      <a
+        id="user-menu-item-2"
+        href="#"
+        class="block px-4 py-2 text-sm text-gray-700"
+        role="menuitem"
+        tabindex="-1"
+      >Sign out</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuShow: false
+    }
+  },
+  methods: {
+    showMenu() {
+      this.isMenuShow = !this.isMenuShow
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/products.vue
@@ -0,0 +1,89 @@
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+  -->
+  <div class="min-h-full">
+    <Navigation />
+
+    <header class="bg-white shadow">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold text-gray-900">Maxsulotlar</h1>
+      </div>
+    </header>
+    <main>
+      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <div class="px-4 py-6 sm:px-0">
+          <!-- This example requires Tailwind CSS v2.0+ -->
+          <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >Name</th>
+                        <th
+                          scope="col"
+                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >Title</th>
+                        <th
+                          scope="col"
+                          class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >Category</th>
+                        <th scope="col" class="relative px-6 py-3">
+                          <span class="sr-only">Edit</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                      <tr>
+                        <td
+                          class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                        >iPhone 12 Pro max</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          Тип устройства
+                          беспроводные наушники
+                          Микрофон
+                          есть
+                        </td>
+
+                        <td class="px-6 py-4 whitespace-nowrap">
+                          <span
+                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                          >Telefonlar</span>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                        </td>
+                      </tr>
+
+                      <!-- More people... -->
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /End replace -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
